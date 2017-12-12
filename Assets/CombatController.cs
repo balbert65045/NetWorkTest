cd6a8411412cb15e56f6401776befed294d76711
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-12-12 08:05:51 -0500

    start

---
+++ Assets/CombatController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour {
+
+    // Use this for initialization
+    Animator m_Animator;
+
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("MeleeAttack1"))
+        {
+            m_Animator.SetTrigger("MeleeAttack1");
+        }
+	}
+
+    public void EnableAttack()
+    {
+        GetComponentInChildren<Weapon>().SetAttackState(true);
+    }
+
+    public void DisableAttack()
+    {
+        GetComponentInChildren<Weapon>().SetAttackState(false);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, .7f);
+    }
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    // Use this for initialization
+    bool AttackStateEnabled = false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetAttackState(bool value)
+    {
+        AttackStateEnabled = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (AttackStateEnabled)
+        {
+            if (other.GetComponentInChildren<Weapon>() != this)
+            {
+                Debug.Log("Triggered with " + other.name);
+            }
+        }
+    }
+}
