befd4ef74a7857d783e30e64b1348bef64bc94d9
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2018-01-02 07:28:05 -0500

    more network changes

--- Assets/myNetworkManager.cs
+++ Assets/myNetworkManager.cs
@@ -8,12 +8,18 @@ public class myNetworkManager : NetworkManager {
     public override void OnStartHost()
     {
         base.OnStartHost();
-        Debug.Log(Time.timeSinceLevelLoad + "Host requested");
+        Debug.Log(Time.timeSinceLevelLoad + " Host requested");
     }
 
     public override void OnStartClient(NetworkClient myClient)
     {
         base.OnStopClient();
         Debug.Log(Time.timeSinceLevelLoad + " Client start requested" );
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log(Time.timeSinceLevelLoad + " Client connected to IP:" + conn.address);
     }
 }
